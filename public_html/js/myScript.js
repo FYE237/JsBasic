cb95ed4ba2da945e5119287aba5845566c63f761
Sébastien Viardot <Sebastien.Viardot@grenoble-inp.fr>
2023-02-07 14:10:15 +0100

    Point de départ

---
+++ public_html/js/myScript.js
@@ -0,0 +1,23 @@
+const cellSize = 20 
+const size = 30
+
+const life = {
+    // A compléter
+}
+
+class Cell {
+// A compléter
+}
+function onKeyUp(event){
+    // A Compléter
+}
+window.addEventListener("keyup",onKeyUp);
+
+window.addEventListener("load",
+    function(){
+        let canvas = document.getElementById("myCanvas")
+        paper.setup(canvas)
+        // Placer le code à exécuter ici pour qu'il le soit une fois la page 
+        // chargée dans son intégralité
+    }
+)
