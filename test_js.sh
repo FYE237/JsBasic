befe1f5b955fa7ddd7ead0c1b83f256f5bc743bc
FEZEU YOUMBI Emmanuel <yfezeu.emma@gmail.com>
2023-02-10 19:26:59 +0100

    TOPIC : GOOD

--- test_js.sh
+++ test_js.sh
@@ -1,8 +1,8 @@
-rm "js_report.txt";
+# rm "js_report.txt";
 
-npx eslint public_html/js/myScript.js >> js_report.txt ;
+# npx eslint public_html/js/myScript.js >> js_report.txt ;
 
-npx eslint *.js >> js_report.txt;
+# npx eslint *.js >> js_report.txt;
 
 NBERR=$(grep -e "error" js_report.txt | wc -l);
 
